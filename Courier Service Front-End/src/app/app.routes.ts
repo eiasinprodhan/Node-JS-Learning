5aece56bed61c53979e68548a60568e6b0cb0921
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-10-26 10:29:50 +0600

    Added

--- Courier Service Front-End/src/app/app.routes.ts
+++ Courier Service Front-End/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AllAddress } from './address/all-address/all-address';
+import { AddAddress } from './address/add-address/add-address';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: AllAddress },
+    { path: 'addAddress', component: AddAddress },
+
+
+];
